98c7b38e8c22883266dc865cf4419e9c95e4738f
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-06-09 17:04:10 +0200

    Extract ugly gtk_layer_shell code

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ use std::{
 };
 
 use gio::prelude::*;
-use gtk::{
-    prelude::*,
-    WidgetExt,
-};
+use gtk::{ApplicationWindow, prelude::*, WidgetExt};
 
 // upgrade weak reference or return
 #[macro_export]
@@ -44,19 +41,7 @@ fn activate(application: &gtk::Application) {
         Inhibit(false)
     });
 
-    gtk_layer_shell::init_for_window(&window);
-    gtk_layer_shell::set_layer(&window, gtk_layer_shell::Layer::Top);
-    gtk_layer_shell::auto_exclusive_zone_enable(&window);
-
-    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Top, 0);
-    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Bottom, 0);
-    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Left, 0);
-    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Right, 0);
-
-    gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Top, true);
-    gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Bottom, false);
-    gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Left, true);
-    gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Right, true);
+    init_layer_shell(&window);
 
     let content_box = gtk::Box::new(gtk::Orientation::Horizontal, 16);
     content_box.set_halign(gtk::Align::Fill);
@@ -64,4 +49,20 @@ fn activate(application: &gtk::Application) {
     window.add(&content_box);
 
     window.show_all();
+}
+
+fn init_layer_shell(window: &ApplicationWindow) {
+    gtk_layer_shell::init_for_window(window);
+    gtk_layer_shell::set_layer(window, gtk_layer_shell::Layer::Top);
+    gtk_layer_shell::auto_exclusive_zone_enable(window);
+
+    gtk_layer_shell::set_margin(window, gtk_layer_shell::Edge::Top, 0);
+    gtk_layer_shell::set_margin(window, gtk_layer_shell::Edge::Bottom, 0);
+    gtk_layer_shell::set_margin(window, gtk_layer_shell::Edge::Left, 0);
+    gtk_layer_shell::set_margin(window, gtk_layer_shell::Edge::Right, 0);
+
+    gtk_layer_shell::set_anchor(window, gtk_layer_shell::Edge::Top, true);
+    gtk_layer_shell::set_anchor(window, gtk_layer_shell::Edge::Bottom, false);
+    gtk_layer_shell::set_anchor(window, gtk_layer_shell::Edge::Left, true);
+    gtk_layer_shell::set_anchor(window, gtk_layer_shell::Edge::Right, true);
 }
