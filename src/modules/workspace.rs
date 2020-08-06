57bd37096762b81f20715271068ff5044a9f6bfc
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-07 01:51:00 +0200

    Include all workspaces in workspaces module

--- src/modules/workspace.rs
+++ src/modules/workspace.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use glib::Continue;
-use gtk::{Label, LabelExt};
+use gtk::{LabelExt, Orientation, ContainerExt, BoxExt, WidgetExt};
 use ksway::{IpcCommand, IpcEvent};
 use serde::Deserialize;
 use tokio::task::{block_in_place, spawn_blocking};
@@ -7,12 +9,17 @@ use tokio::task::{block_in_place, spawn_blocking};
 use crate::modules::module::Module;
 
 #[derive(Deserialize)]
-pub struct WorkspaceModule {}
+pub struct WorkspaceModule {
+    #[serde(default = "default_zero")]
+    spacing: i32
+}
 
-#[derive(Deserialize)]
+fn default_zero() -> i32 { 0 }
+
+#[derive(Deserialize, Debug)]
 struct WorkspaceEvent {
     change: String,
-    // old: Option<IPCWorkspace>,
+    old: Option<Workspace>,
     current: Option<Workspace>,
 }
 
@@ -20,23 +27,39 @@ struct WorkspaceEvent {
 struct Workspace {
     name: String,
     focused: bool,
+    num: usize,
 }
 
-impl Module<gtk::Label> for WorkspaceModule {
-    fn into_widget(self) -> Label {
-        let content = Label::new(None);
+impl Workspace {
+    fn as_label(&self) -> gtk::Label {
+        let label = gtk::Label::new(Some(self.name.as_str()));
+        label
+    }
+}
+
+impl Module<gtk::Box> for WorkspaceModule {
+    fn into_widget(self) -> gtk::Box {
+        let content = gtk::Box::new(Orientation::Horizontal, self.spacing);
+
+        let mut wp_map = HashMap::new();
 
         let mut sway = match ksway::client::Client::connect() {
             Ok(client) => client,
             Err(e) => panic!("Couldn't connect to sway: {}", e)
         };
 
-        let _ = block_in_place(|| {
+        if let Ok(workspaces) = block_in_place(|| {
             let wp = sway.ipc(IpcCommand::GetWorkspaces)?;
             let wp = String::from_utf8(wp).unwrap();
             let wp: Vec<Workspace> = serde_json::from_str(wp.as_str()).unwrap();
             Ok::<Vec<Workspace>, ksway::Error>(wp)
-        });
+        }) {
+            for wp in workspaces {
+                let label = wp.as_label();
+                content.add(&label);
+                wp_map.insert(wp.num, label);
+            }
+        }
 
         let srx = sway.subscribe(vec![IpcEvent::Workspace]).unwrap();
         let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
@@ -46,10 +69,9 @@ impl Module<gtk::Label> for WorkspaceModule {
                     // payload_type is always going to be workspace since it's the only event we subscribed to,
                     // but if we subscribe to something else in the future please also check payload_type
                     let payload = String::from_utf8(payload).unwrap();
+                    // println!("{}", payload);
                     let payload: WorkspaceEvent = serde_json::from_str(payload.as_str()).unwrap();
-                    if payload.change == "focus" {
-                        let _ = tx.send(payload.current.unwrap().name);
-                    }
+                    tx.send(payload).expect("Couldn't send workspace payload");
                 }
                 sway.poll().unwrap() // unwrap explicitly -> panic if polling fails
             }
@@ -57,8 +79,39 @@ impl Module<gtk::Label> for WorkspaceModule {
 
         {
             let content = content.clone();
-            rx.attach(None, move |name| {
-                content.set_text(name.as_str());
+            rx.attach(None, move |event| {
+                match event.change.as_ref() {
+                    "focus" => {
+                        let old = event.old.unwrap();
+                        wp_map.get(&old.num).unwrap().set_text(old.name.as_str());
+
+                        let current = event.current.unwrap();
+                        let current_label = wp_map.get(&current.num).unwrap();
+                        current_label.set_text(format!(" F{}F ", current.name).as_str());
+
+                        if current_label.get_parent().is_none() {
+                            content.add(current_label);
+                        }
+                    }
+                    "init" => {
+                        let wp = event.current.unwrap();
+                        let label = wp.as_label();
+                        content.pack_start(
+                            &label,
+                            false,
+                            true,
+                            0
+                        );
+                        label.show();
+                        wp_map.insert(wp.num, label);
+                    }
+                    "empty" => {
+                        let label = wp_map.get(&event.current.unwrap().num).unwrap();
+                        content.remove(label);
+                    }
+                    _ => ()
+                }
+
                 Continue(true)
             });
         }
